088b4cf894707c7161ae71671e119081bf641794
luke <lukehampton6@gmail.com>
2022-06-24 00:57:29 -0400

    input for api call added

--- src/components/PokeApi.js
+++ src/components/PokeApi.js
@@ -1,16 +1,18 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 function PokeApi() {
     const [data, setData] =useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [name, setName] = useState(null);
+    const nameRef = useRef();
 
     useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon-species/bulbasaur/')
+        fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}/`)
         .then((response) => {
             if (!response.ok) {
               throw new Error(
-                `This is an HTTP error: The status is ${response.status}`
+                'no pokemon found :('
               );
             }
             return response.json();
@@ -26,26 +28,41 @@ function PokeApi() {
           .finally(() => {
             setLoading(false);
           });
-    }, []);
-    
+    });
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        setName(nameRef.current.value);
+    }
+
     return (
         <div>
-        <h1>Pokemon</h1>
-      {loading && <div>A moment please...</div>}
-      {error && (
-        <div>{`There is a problem fetching the post data - ${error}`}</div>
-      )}
-      <ul>
-        {data && (
-            <ul>
-                <li>{data.name}</li>
-                <li>{data.id}</li>
-                <li>{data.generation.name}</li>
-                <li>{data.flavor_text_entries[0].flavor_text}</li>
-            </ul>
+         <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="name">Name</label>
+                <input
+                id="name"
+                type="text"
+                ref={nameRef}
+                />
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+        {loading && <div>A moment please...</div>}
+        {error && (
+            <div>{`There is a problem fetching the post data - ${error}`}</div>
         )}
-      </ul>
-      </div>
+        <ul>
+            {data && (
+                <ul>
+                    <li>{data.name}</li>
+                    <li>{data.id}</li>
+                    <li>{data.generation.name}</li>
+                    <li>{data.flavor_text_entries[0].flavor_text}</li>
+                </ul>
+            )}
+        </ul>
+        </div>
     )
 }
 
